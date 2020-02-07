3d3492a0720f7c64adbf5139d03cb078b5e99084
Tuan Anh Tran <me@tuananh.org>
2020-02-07 16:41:26 +0700

    add byline

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ addEventListener('fetch', event => {
     event.respondWith(fetchAndRender(event.request))
 })
 
-const toBeRemovedTags = ['head', 'script', 'style', 'noscript', 'iframe']
+const toBeRemovedTags = ['link', 'script', 'style', 'noscript', 'iframe']
 // attempt to use htmlrewriter to cleanup the html first.
 class ElementHandler {
     element(element) {
@@ -20,7 +20,7 @@ class ElementHandler {
 }
 
 // without sanitize :-o
-const render = (parsed) =>  `<!DOCTYPE html>
+const render = parsed => `<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -70,6 +70,7 @@ const render = (parsed) =>  `<!DOCTYPE html>
         </hr>
 
         <h2>${parsed.title}</h2>
+        <strong>${parsed.byline}</strong>
         ${parsed.content}
     </body>
 </html>
@@ -77,19 +78,26 @@ const render = (parsed) =>  `<!DOCTYPE html>
 
 const htmlHeaders = { 'Content-Type': 'text/html; charset=utf-8' }
 const fetchHeaders = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 }
 async function fetchAndRender(request) {
-    const url = new URL(request.url).searchParams.get('url')    
+    const url = new URL(request.url).searchParams.get('url')
     if (!url) {
         const responseInit = {
             status: 400,
             headers: htmlHeaders
         }
-        return new Response('Error: missing url. Eg: https://reader.tuananh.net/?url=https://www.theverge.com/2020/2/5/21124201/bill-simmons-the-ringer-spotify-acquistion-podcast-purchase', responseInit)
+        return new Response(
+            'Error: missing url. Eg: https://reader.tuananh.net/?url=https://www.theverge.com/2020/2/5/21124201/bill-simmons-the-ringer-spotify-acquistion-podcast-purchase',
+            responseInit
+        )
     }
 
-    const oldResp = await fetch(url, {...fetchHeaders, cf: { cacheTtl: 3000 }})
+    const oldResp = await fetch(url, {
+        ...fetchHeaders,
+        cf: { cacheTtl: 3000 }
+    })
     const newResp = new HTMLRewriter()
         .on('*', new ElementHandler())
         .transform(oldResp)
@@ -101,14 +109,14 @@ async function fetchAndRender(request) {
         status: 200,
         headers: htmlHeaders
     }
-    
+
     // return new Response(text, responseInit)
 
     const doc = new JSDOMParser().parse(text, 'http://fakehost/')
     const reader = new Readability(doc, {
         charThreshold: 2000
     })
-    const article = reader.parse()    
+    const article = reader.parse()
 
     return new Response(render(article), responseInit)
 }
