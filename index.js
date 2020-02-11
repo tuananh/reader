cafba38bae3f753abb9097cce604a9ba8c459462
Tuan Anh Tran <me@tuananh.org>
2020-02-11 09:32:46 +0700

    fix: image url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Readability = require('readability')
 const { JSDOMParser } = require('readability/JSDOMParser')
+const render = require('./render')
 
 addEventListener('fetch', event => {
     event.respondWith(fetchAndRender(event.request))
@@ -19,63 +20,6 @@ class ElementHandler {
     }
 }
 
-// without sanitize :-o
-const render = parsed => `<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>${parsed.title}</title>
-        <meta name="description" content="${parsed.excerpt}">
-        <style>
-            body {
-                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
-                line-height: 1.6;
-                color: #222;
-                max-width: 40rem;
-                padding: 2rem;
-                margin: auto;
-                background: #fafafa;
-            }
-
-            img {
-                max-width: 100%;
-            }
-
-            a {
-                color: #2ECC40;
-            }
-
-            h1,
-            h2,
-            strong {
-                color: #111;
-            }
-            @media (prefers-color-scheme: dark) {
-                body {
-                    background-color: black;
-                    color: #ccc;
-                }
-                h1, h2, strong {
-                    color: #e0e0e0;
-                }
-            }
-        </style>
-    </head>
-    <body>
-        <div style="text-align: center">
-            <h2 style="text-align: center">A reader mode sharable url built with Cloudflare workers</h2 style="align: center">
-            <i>If you find a bug with the generated text, please find an issue over <a href="https://github.com/tuananh/reader/issues/new" target="_blank" rel="noopener">GitHub</a>.</i>
-        </div>
-        </hr>
-
-        <h2>${parsed.title}</h2>
-        <strong>${parsed.byline}</strong>
-        ${parsed.content}
-    </body>
-</html>
-`
-
 const htmlHeaders = { 'Content-Type': 'text/html; charset=utf-8' }
 const fetchHeaders = {
     'User-Agent':
@@ -83,6 +27,7 @@ const fetchHeaders = {
 }
 async function fetchAndRender(request) {
     const url = new URL(request.url).searchParams.get('url')
+    
     if (!url) {
         const responseInit = {
             status: 400,
@@ -93,7 +38,7 @@ async function fetchAndRender(request) {
             responseInit
         )
     }
-
+    const parsedUrl = new URL(url)
     const oldResp = await fetch(url, {
         ...fetchHeaders,
         cf: { cacheTtl: 3000 }
@@ -112,7 +57,7 @@ async function fetchAndRender(request) {
 
     // return new Response(text, responseInit)
 
-    const doc = new JSDOMParser().parse(text, 'http://fakehost/')
+    const doc = new JSDOMParser().parse(text, parsedUrl.protocol + '//' + parsedUrl.host)
     const reader = new Readability(doc, {
         charThreshold: 2000
     })
---
+++ render.js
@@ -0,0 +1,57 @@
+const render = parsed => `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>${parsed.title}</title>
+        <meta name="description" content="${parsed.excerpt}">
+        <style>
+            body {
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+                line-height: 1.6;
+                color: #222;
+                max-width: 40rem;
+                padding: 2rem;
+                margin: auto;
+                background: #fafafa;
+            }
+
+            img {
+                max-width: 100%;
+            }
+
+            a {
+                color: #2ECC40;
+            }
+
+            h1,
+            h2,
+            strong {
+                color: #111;
+            }
+            @media (prefers-color-scheme: dark) {
+                body {
+                    background-color: black;
+                    color: #ccc;
+                }
+                h1, h2, strong {
+                    color: #e0e0e0;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div style="text-align: center">
+            <h2 style="text-align: center">A reader mode sharable url built with Cloudflare workers</h2 style="align: center">
+            <i>If you find a bug with the generated text, please find an issue over <a href="https://github.com/tuananh/reader/issues/new" target="_blank" rel="noopener">GitHub</a>.</i>
+        </div>
+        </hr>
+
+        <h2>${parsed.title}</h2>
+        <strong>${parsed.byline}</strong>
+        ${parsed.content}
+    </body>
+</html>
+`
+
+module.exports = render
